fa39f639ea68f173455deef2c4fce36f4394bfb5
David Aronsohn <bitm@slickdeals.net>
2015-04-30 17:28:52 -0700

    Initial Cookbook

---
+++ README.md
@@ -0,0 +1,4 @@
+# chef-server-backup
+
+TODO: Enter the cookbook description here.
+
---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'chef-server-backup'
+maintainer       'The Authors'
+maintainer_email 'you@example.com'
+license          'all_rights'
+description      'Installs/Configures chef-server-backup'
+long_description 'Installs/Configures chef-server-backup'
+version          '0.1.0'
+
